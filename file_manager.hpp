eb6dbfd469c609b3c50db9d154a22179de4a5f00
Joshua Dahl <joshuadahl@unr.edu>
2026-01-17 07:53:06 -0800

    Grammar Validator
    Validate that DOIR's grammar works.

---
+++ file_manager.hpp
@@ -0,0 +1,31 @@
+
+#include <cstddef>
+#include <filesystem>
+#include <unordered_map>
+#include <span>
+
+#include "mio.hpp"
+
+struct file_manager {
+	static file_manager& singleton() {
+		static file_manager m;
+		return m;
+	}
+
+	std::unordered_map<std::filesystem::path, mio::mmap_source> mapped_files;
+
+	mio::mmap_source& load_file(const std::filesystem::path& path) {
+		if(mapped_files.contains(path)) return mapped_files[path];
+		return mapped_files[path] = path.string();
+	}
+
+	std::span<std::byte> get_file_bytes(std::filesystem::path path) {
+		auto& mio = load_file(path);
+		return {(std::byte*)mio.data(), mio.size()};
+	}
+
+	std::string_view get_file_string(std::filesystem::path path) {
+		auto& mio = load_file(path);
+		return {mio.data(), mio.size()};
+	}
+};
