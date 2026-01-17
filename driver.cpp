18aebed88967493c715789c8b04d8124519f6035
Joshua Dahl <joshuadahl@unr.edu>
2026-01-17 07:53:06 -0800

    Grammar Validator
    Validate that DOIR's grammar works.

--- driver.cpp
+++ driver.cpp
@@ -1,5 +1,31 @@
+#include <filesystem>
 #include <iostream>
 
+#include <peglib.h>
+#include <string_view>
+
+#include "file_manager.hpp"
+
 int main() {
-    std::cout << "Hello World" << std::endl;
+	peg::parser parser;
+
+	parser.set_logger([](size_t line, size_t col, const std::string& msg, const std::string &rule) {
+		std::cerr << line << ":" << col << ": " << msg << std::endl;
+	});
+
+	auto grammar =
+#include "grammar.peg"
+	;
+	auto ok = parser.load_grammar(grammar);
+	assert(ok);
+
+	auto test = file_manager::singleton().get_file_string("../standard.doir");
+
+	parser.enable_ast();
+
+	std::shared_ptr<peg::Ast> ast;
+	if (parser.parse(test, ast)) {
+		// ast = parser.optimize_ast(ast);
+		std::cout << peg::ast_to_s(ast) << std::endl;
+	}
 }
